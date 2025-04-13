dc7044202a0f2260ea5ea10fff2ae613e9ae680c
SamuMartinez94 <samu.smc94@gmail.com>
2025-04-13 12:03:49 +0200

    Prueba

--- app/src/main/java/com/example/ratestationgrupo3/MainActivity.java
+++ app/src/main/java/com/example/ratestationgrupo3/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
 
-
+        //Hola, hacer pruebas de subir algo
         });
     }
 }
