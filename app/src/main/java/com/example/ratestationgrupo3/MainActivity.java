51af54d13d5f2e25a0b4648f2219806d52c3ef80
SamuMartinez94 <samu.smc94@gmail.com>
2025-04-13 13:20:46 +0200

    Probar que os funciona bien

--- app/src/main/java/com/example/ratestationgrupo3/MainActivity.java
+++ app/src/main/java/com/example/ratestationgrupo3/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
 
-        //Hola, hacer pruebas de subir como
+        //Hola, hacer alguna prueba como esta para ver que podeis editar.
 
         });
     }
