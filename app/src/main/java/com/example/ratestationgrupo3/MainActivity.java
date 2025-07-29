9a3de6a486965049f494634ec95a78431d011bdb
SamuMartinez94 <samu.smc94@gmail.com>
2025-07-29 15:04:19 +0200

    Vuelta de vacaciones

--- app/src/main/java/com/example/ratestationgrupo3/MainActivity.java
+++ app/src/main/java/com/example/ratestationgrupo3/MainActivity.java
@@ -20,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
 
-        //Hola, hacer alguna prueba como esta para ver que podeis editar.
+        //Hola
+            //prueba
 
         });
     }
